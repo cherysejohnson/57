3720f555b45e54305d54b4ac2a5aadea43c146a5
cherysejohnson <cherysejohnson@gmail.com>
2023-02-22 09:55:29 -0500

    solved 8

--- 2 - Calculations/eight/eight.js
+++ 2 - Calculations/eight/eight.js
@@ -1,3 +1,9 @@
 party = () => {
-    
-}
+    let people = parseInt(document.getElementById('people').value, 10);
+    let pizzas = parseInt(document.getElementById('pizzas').value, 10);
+    let slices = parseInt(document.getElementById('slices').value, 10);
+    let math = Math.floor((slices * pizzas) / people);
+    let leftovers = ((slices * pizzas) % ((slices * pizzas) / people)).toFixed(2);
+    document.getElementById('result').innerHTML = `There are ${people} people attending a party with ${pizzas} pies. Each person gets ${math} slices. There are ${leftovers} slices left over.`;
+    document.forms['start'].reset();
+};
