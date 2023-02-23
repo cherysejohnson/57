f801effe8d2a9d9acae87f7e6380a1665bdda97e
cherysejohnson <cherysejohnson@gmail.com>
2023-02-23 15:45:59 -0500

    unfinished nine - styles done, no js

---
+++ 2 - Calculations/nine/nine.js
@@ -0,0 +1,6 @@
+gallons = () => {
+    let length = parseInt(document.getElementById('length').value, 10);
+    let width = parseInt(document.getElementById('width').value, 10);
+    let num = length * width;
+    if ((num / 350) % )
+}
