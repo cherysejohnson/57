6bcccd40ec5688d85fcfbc1839e73380c8712d65
cherysejohnson <cherysejohnson@gmail.com>
2023-02-27 13:19:54 -0500

    Please enter the commit message for your changes. Lines starting

--- 2 - Calculations/nine/nine.js
+++ 2 - Calculations/nine/nine.js
@@ -1,6 +1,7 @@
 gallons = () => {
     let length = parseInt(document.getElementById('length').value, 10);
     let width = parseInt(document.getElementById('width').value, 10);
+    document.forms['start'].reset();
     let num = length * width;
     // focus on how you would always know the remainder & ratio
 }
